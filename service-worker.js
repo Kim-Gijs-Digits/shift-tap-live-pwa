8947cf60692c8d4fc1903ce91c49efa30eb941a2
Kim Gijs <shifttap.app@gmail.com>
2026-03-20 21:10:23 +0100

    Fix offline cache in live build

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'shift-tap-v1.0.0';
+const CACHE_VERSION = 'shift-tap-v1.0.1';
 const APP_SHELL_CACHE = `app-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 
@@ -14,7 +14,8 @@ const APP_SHELL_FILES = [
   './jspdf.umd.min.js',
   './service-worker.js',
   './lock_open.svg',
-  './lock_close.svg'
+  './lock_close.svg',
+  './notifications.js',
 ];
 
 self.addEventListener('install', (event) => {
